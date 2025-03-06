727548eecbf413b7bebb3a8bf5f5af70fb192ca5
krivosheevvv <krivosheevvv@expocentr.ru>
2025-03-06 14:30:26 +0300

    created first view of chat, added flowbite/tailwind, decompose

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,11 @@
 <script setup lang="ts">
-//@ts-ignore
-import Chat from "./components/Chat.vue";
+import Chat from '@/components/Chat.vue'
 </script>
 
 <template>
-  <div>
-    <h1 class="text-center text-2xl font-bold my-4">WebSocket Chat</h1>
+  <div class="h-full bg-gray-50 dark:bg-gray-700">
     <Chat />
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- frontend/src/components/Chat.vue
+++ frontend/src/components/Chat.vue
@@ -1,62 +1,49 @@
 <template>
-  <div class="chat-container">
-    <div class="messages">
-      <div v-for="(message, index) in messages" :key="index" class="message">
-        <strong>{{ message.user }}:</strong> {{ message.text }}
-      </div>
+  <div
+    class="max-w-[800px] mx-auto rounded-lg bg-gray-100 dark:bg-gray-800 h-full flex flex-col p-4"
+  >
+    <h1 class="text-center text-2xl font-bold my-4">MyChat</h1>
+    <div ref="chatContainer" class="h-150 max-h-[calc(100vh-6.25rem)] overflow-auto flex-1">
+      <h3 class="text-center text-gray-500 font-medium">Start your chatting with strangers here</h3>
+      <ChatMessageList :messages="messages" />
     </div>
-    <input v-model="newMessage" @keyup.enter="sendMessage" placeholder="Type a message..." />
+    <ChatForm @sendMessage="messageSend" />
   </div>
 </template>
 
-<script>
-import { ref, onMounted } from 'vue';
-import { io } from 'socket.io-client';
+<script setup lang="ts">
+import ChatMessageList from '@/components/ChatMessageList.vue'
+import ChatForm from '@/components/ChatForm.vue'
+import { ref, onMounted, nextTick } from 'vue'
+import { io } from 'socket.io-client'
+import type { IMessage } from '@/type.ts'
+import type { Ref } from 'vue'
 
-export default {
-  setup() {
-    const socket = io('http://localhost:3000');
-    const messages = ref([]);
-    const newMessage = ref('');
+const socket = io('http://localhost:3000')
+const messages: Ref<IMessage[]> = ref([])
 
-    const sendMessage = () => {
-      if (newMessage.value.trim()) {
-        socket.emit('sendMessage', {  user: 'You', text: newMessage.value });
-        newMessage.value = '';
-      }
-    };
+const messageSend = (text: string) => {
+  const msg: IMessage = {
+    text,
+    user: 'You',
+  }
+  socket.emit('sendMessage', msg)
+}
+const chatContainer = ref<HTMLElement | null>(null)
+const scrollToBottom = (elem: Ref<HTMLElement | null>) => {
+  if (elem.value) {
+    elem.value.scrollTop = elem.value.scrollHeight
+  }
+}
 
-    onMounted(() => {
-      socket.on('newMessage', (message) => {
-        messages.value.push(message);
-      });
-    });
-
-    return { messages, newMessage, sendMessage };
-  },
-};
+onMounted(() => {
+  socket.on('newMessage', (message: IMessage) => {
+    messages.value.push(message)
+    nextTick(() => {
+      scrollToBottom(chatContainer)
+    })
+  })
+})
 </script>
 
-<style scoped>
-.chat-container {
-  max-width: 400px;
-  margin: 0 auto;
-  padding: 20px;
-  border: 1px solid #ccc;
-  border-radius: 8px;
-}
-.messages {
-  height: 300px;
-  overflow-y: auto;
-  margin-bottom: 10px;
-}
-.message {
-  margin-bottom: 8px;
-}
-input {
-  width: 100%;
-  padding: 8px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-}
-</style>
+<style scoped></style>
